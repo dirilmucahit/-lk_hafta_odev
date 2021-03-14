9a762f21c09b64e30b96dd89c20c3eeac6020896
Meryem-Tayar <80575731+Meryem-Tayar@users.noreply.github.com>
2021-03-14 13:19:00 +0300

    Update ilkodev.java

--- src/ilkodev.java
+++ src/ilkodev.java
@@ -1,5 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Merhaba Dünya");    }
+	System.out.println("Merhaba Dünya"); 
+         System.out.println("Hello wORLD :) MERYEM"); 
+    }
 }
